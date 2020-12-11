eb62f546495da31dc51b05a8ee3c167e90d0a058
Guillaume Dupont <g.dupont528@gmail.com>
2020-12-11 16:15:08 +0100

    menu

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-const quote = document.querySelector('#quote');
-const author = document.querySelector('#author');
+const quoteUI = document.querySelector('#quote');
+const authorUI = document.querySelector('#author');
 const button = document.querySelector('.newQuote Button');
+const menu = document.querySelector('#menu');
+const sidebar = document.querySelector('.sidebar');
+const hollowHeart = document.querySelector('#hollowHeart i');
 
-getNewQuote(){
-  let id;
-  id = Math.floor(Math.random()*31 + 1 );
-  console.log(id);
+function showMenu() {
+  sidebar.classList.toggle('show');
 }
 
-getNewQuote();
+function fillHeart() {
+  hollowHeart.classList.toggle('far');
+  hollowHeart.classList.toggle('fas');
+}
+
+function getNewQuote() {
+  // let id;
+  // id = Math.floor(Math.random() * 31 + 1);
+  // console.log(id);
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'quotes.json', true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(this.responseText);
+
+      const quotes = JSON.parse(this.responseText);
+
+      console.log(quotes);
+
+      quoteUI.textContent = `"${quotes[0].quote}"`;
+      authorUI.textContent = `${quotes[0].author}`;
+    }
+  };
+
+  xhr.send();
+}
+
+// let storedQuote;
+
+// localStorage.setItem('quote', 'I am groot');
+// localStorage.setItem('author', 'Groot');
+// storedQuote = localStorage.getItem('quote');
+// storedAuthor = localStorage.getItem('author');
+
+// console.log(storedQuote);
+// console.log(storedAuthor);
+
+// localStorage.clear();
 
 // Event Listeners
-button.addEventListener('click', getNewQuote)
+button.addEventListener('click', getNewQuote);
+menu.addEventListener('click', showMenu);
+hollowHeart.addEventListener('click', fillHeart);
