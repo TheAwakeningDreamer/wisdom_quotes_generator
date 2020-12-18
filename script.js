c9bd104fa56028cecc8915651f15b0758c6e571f
Guillaume Dupont <g.dupont528@gmail.com>
2020-12-18 10:29:17 +0100

    cardsgit status

--- script.js
+++ script.js
@@ -8,49 +8,72 @@ const hollowHeart = document.querySelector('#hollowHeart i');
 const favQuotesTitle = document.querySelector('.fav-container h2');
 const favQuotes = document.querySelector('.fav-quotes');
 
-// const x = matchMedia('(max-width:760px');
-
-// function mediaQueries() {
-//   if (x.matches) {
-//     menu.document.getElementByTagName('i').className = 'fas fa-share fa-2x';
-//   }
-// }
-
-window.onload = function () {
-  getNewQuote();
-};
-
+// Show Menu when click
 function showMenu() {
   sidebar.classList.toggle('show');
 }
 
+// Add to favorite
 function addToFavorites(e) {
+  // Change icon
   hollowHeart.classList.toggle('far');
   hollowHeart.classList.toggle('fas');
-  const newFavQuote =
-    button.parentElement.previousElementSibling.lastElementChild.children[0]
-      .textContent;
-  const newFavAuthor =
-    button.parentElement.previousElementSibling.lastElementChild.children[1]
-      .textContent;
 
-  let quotes;
-  if (localStorage.getItem('quotes') === null) {
-    quotes = [];
-  } else {
-    quotes = JSON.parse(localStorage.getItem('quotes'));
-  }
-  quotes.push(newFavQuote);
-  localStorage.setItem('quotes', JSON.stringify(quotes));
+  if (hollowHeart.classList.contains('fas')) {
+    const newFavQuote =
+      button.parentElement.previousElementSibling.lastElementChild.children[0]
+        .textContent;
+    const newFavAuthor =
+      button.parentElement.previousElementSibling.lastElementChild.children[1]
+        .textContent;
+    // Store in Local Storage
+    let quotes;
+    if (localStorage.getItem('quotes') === null) {
+      quotes = [];
+    } else {
+      quotes = JSON.parse(localStorage.getItem('quotes'));
+    }
+    quotes.push(newFavQuote);
+    localStorage.setItem('quotes', JSON.stringify(quotes));
 
-  let authors;
-  if (localStorage.getItem('authors') === null) {
-    authors = [];
-  } else {
-    authors = JSON.parse(localStorage.getItem('authors'));
+    let authors;
+    if (localStorage.getItem('authors') === null) {
+      authors = [];
+    } else {
+      authors = JSON.parse(localStorage.getItem('authors'));
+    }
+    authors.push(newFavAuthor);
+    localStorage.setItem('authors', JSON.stringify(authors));
+    console.log(quotes);
+    console.log(authors);
+
+    // Create Cards
+    // Create individual card
+    let card = document.createElement('div');
+    card.className = 'quote-box';
+    card.id = 'quote-box';
+
+    // Create blockquote & cite + append to div
+    let blockquote = document.createElement('blockquote');
+    blockquote.id = 'quote';
+    let cite = document.createElement('cite');
+    cite.id = 'author';
+    card.appendChild(blockquote);
+    card.appendChild(cite);
+
+    // Quote
+    quotes.forEach(function (quote) {
+      blockquote.textContent = `${quote}`;
+    });
+
+    authors.forEach(function (author) {
+      cite.textContent = `${author}`;
+    });
+
+    console.log(card);
+
+    favQuotes.appendChild(card);
   }
-  authors.push(newFavAuthor);
-  localStorage.setItem('authors', JSON.stringify(authors));
 
   e.preventDefault();
 }
@@ -64,49 +87,6 @@ function showFav() {
   } else {
     favBtn.textContent = 'Show Favs';
   }
-
-  const quotes = JSON.parse(localStorage.getItem('quotes'));
-  const authors = JSON.parse(localStorage.getItem('authors'));
-
-  // Create cards in favQuotes section
-
-  // Create individual Card
-  let card = document.createElement('div');
-  card.className = 'quote-box';
-  card.id = 'quote-box';
-  console.log(card);
-
-  // Create blockquote & append
-  let blockquote = document.createElement('blockquote');
-  blockquote.id = 'quote';
-  card.appendChild(blockquote);
-
-  // Create cite & append
-  let cite = document.createElement('cite');
-  cite.id = 'author';
-  card.appendChild(cite);
-
-  // Append Card to favquotes
-
-  let cards = favQuotes.card;
-
-  function createNewCard() {
-    quotes.forEach(function (quote) {
-      blockquote.innerHTML += `
-                ${quote}
-            `;
-    });
-    authors.forEach(function (author) {
-      cite.innerHTML += `
-        ${author}
-              `;
-    });
-    favQuotes.appendChild(card);
-  }
-
-  createNewCard();
-
-  console.log(card);
 }
 
 function getNewQuote() {
@@ -137,3 +117,4 @@ button.addEventListener('click', getNewQuote);
 menu.addEventListener('click', showMenu);
 hollowHeart.addEventListener('click', addToFavorites);
 favBtn.addEventListener('click', showFav);
+document.addEventListener('DOMContentLoaded', getNewQuote);
